203ec3c65daa63858a94aebf4a95cc772ab1879a
Kaustubh V <kaustubh.v@spit.ac.in>
2021-05-22 16:57:16 +0530

    Initial Commit

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+ 
+ # Class to hold all the details of the task
+class Task(models.Model):
+    title = models.CharField(max_length = 75)
+    description = models.TextField()
+    date = models.DateTimeField(default = timezone.now)
+ 
+    def __str__(self):
+        return self.title
+# Create your models here.
